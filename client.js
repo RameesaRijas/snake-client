2e9b7258f21302f4f6c6bc9513abe60f2a014cbf
RamzRjs <60762747+RameesaRijas@users.noreply.github.com>
2021-09-22 12:45:49 -0400

    snake Initial

--- client.js
+++ client.js
@@ -12,6 +12,11 @@ const connect = function () {
   conn.on('data', (data) => {
     console.log('Server says: ', data);
   });
+
+  conn.on('connect', () => {
+    console.log("Successfully connected to server");
+    conn.write("Name: rjs" );
+  });
   
   return conn;
 };
